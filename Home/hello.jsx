c2368d6ce7d3d4d85c767c5b19daca976f5b2ad2
yltang113 <taoyaoou@gmail.com>
2024-12-02 18:10:26 +0800

    新建hello文件

---
+++ Home/hello.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+export class hello extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+        <p>11111</p>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  
+})
+
+const mapDispatchToProps = {
+  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(hello)
